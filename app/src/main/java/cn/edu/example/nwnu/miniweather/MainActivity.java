4b301b2e5e4b9a964eea477998a00186b607f7b0
wuwenchun <496194722@qq.com>
2016-07-09 15:49:51 +0800

    testnetwork

--- app/src/main/java/cn/edu/example/nwnu/miniweather/MainActivity.java
+++ app/src/main/java/cn/edu/example/nwnu/miniweather/MainActivity.java
@@ -2,6 +2,10 @@ package cn.edu.example.nwnu.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import pack.NetUtil;
 
 /**
  * Created by zzp on 2016/7/9.
@@ -11,5 +15,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this,"网络OK！", Toast.LENGTH_LONG).show();
+        }else
+        {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了！", Toast.LENGTH_LONG).show();
+        }
     }
 }
